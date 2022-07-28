9f0a287af10dbac50cb6f9eebc14343059db40a7
Jeff Schwab <jeff@unbuggy.com>
2022-07-28 15:38:50 -0400

    Show and prevent dangling suffix in Rust and C++

--- safety/src/lib.rs
+++ safety/src/lib.rs
@@ -35,18 +35,18 @@ mod tests {
 
     #[test]
     fn test_make_appender() {
-        let append45 = make_appender(&[3, 4]);
-        assert_eq!(vec![1, 2, 3, 4], append45(vec![1, 2]));
+        let append34 = make_appender(&[3, 4]);
+        assert_eq!(vec![1, 2, 3, 4], append34(vec![1, 2]));
     }
 
     // Won't compile.
     #[test]
     #[rustfmt::skip]
     fn test_make_appender_dangle() {
-    //  let append45 = {
+    //  let append34 = {
     //      let suffix = vec![3, 4];
     //      make_appender(&suffix)
     //  };
-    //  assert_eq!(vec![1, 2, 3, 4], append45(vec![1, 2]));
+    //  assert_eq!(vec![1, 2, 3, 4], append34(vec![1, 2]));
     }
 }
--- safety/src/safety.cpp
+++ safety/src/safety.cpp
@@ -10,6 +10,18 @@ List append(List&& items, List const& suffix) {
     return items;
 }
 
+auto make_appender(List const& suffix) {
+    return [&](List&& items) {
+        return append(move(items), suffix);
+    };
+}
+
+auto make_appender_move(List&& suffix) {
+    return [suffix = move(suffix)](List&& items) {
+        return append(move(items), suffix);
+    };
+}
+
 void test_append() {
     assert((List{1, 2, 3, 4} == append({1, 2}, {3, 4})));
 }
@@ -27,11 +39,30 @@ void test_append_reuse() {
     List items{1};
 //  assert((List{1, 2} == append(items, {2})));        // Won't compile.
     assert((List{1, 2} == append(move(items), {2})));  // OK
-//  assert((List{1, 2} == append(move(items), {2})));  // Failure!
+//  assert((List{1, 2} == append(move(items), {2})));  // FAIL
+}
+
+void test_make_appender() {
+    List suffix{3, 4};
+    auto append34 = make_appender(suffix);
+    assert((List{1, 2, 3, 4} == append34({1, 2})));
+}
+
+void test_make_appender_dangle() {
+    auto append34 = make_appender({3, 4});
+    assert((List{1, 2, 3, 4} == append34({1, 2}))); // FAIL: Undefined behavior
+}
+
+void test_make_appender_move() {
+    auto append34 = make_appender_move({3, 4});
+    assert((List{1, 2, 3, 4} == append34({1, 2}))); // OK
 }
 
 int main() {
     test_append();
     test_append_repeatedly();
     test_append_reuse();
+    test_make_appender();
+//  test_make_appender_dangle();
+    test_make_appender_move();
 }
