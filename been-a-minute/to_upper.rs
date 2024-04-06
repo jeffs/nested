24477a40175de510c4caf6168f0e4d1ed8a3fe93
Jeff Schwab <jeff@wieldy.ai>
2024-04-06 14:49:04 -0400

    Remove explicit flush

--- been-a-minute/to_upper.rs
+++ been-a-minute/to_upper.rs
@@ -1,15 +1,11 @@
-/// Upper cases each line of UTF-8 text from r, and writesit to w.
+/// Upper cases text from r, and writes it to w.
 fn to_upper<R: BufRead, W: Write>(
     mut r: R,
     w: &mut W,
 ) -> io::Result<()> {
     let line = &mut String::new();
     while r.read_line(line)? != 0 {
-        let result = w.write(line.to_uppercase().as_bytes());
-        if result.is_err() {
-            let _ = w.flush();
-            result?;
-        }
+        w.write(line.to_uppercase().as_bytes())?;
     }
     Ok(())
 }
